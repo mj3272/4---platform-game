c42bd8df5210bcd59e8f2dfccc7eab01b78c4113
matiascrespo383 <matiascrespo2002@gmail.com>
2022-09-11 19:41:46 -0700

    level_and_graphics

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,17 +17,26 @@ public class PlayerMovement : MonoBehaviour
     // to add ground as a layer
     [SerializeField] private LayerMask groundLayer;
 
+    // Will be updated with player input
+    private float horizontalInput = 0f;
+
+    // Creating reference to animator - Matias
+    private Animator anim;
+
+
     private void Awake()
     {
         // will check the player object if it has a rigid body component
         // and then will store the component in rbody variable
         rbody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        // Stores the animator in the anim variable - Matias
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // x coordinate value
+        horizontalInput = Input.GetAxis("Horizontal"); // x coordinate value
 
         // use velocity to change how fast player moves and in what direction
         // Vector2 to change movement in 2 directions
@@ -37,20 +46,25 @@ public class PlayerMovement : MonoBehaviour
         rbody.velocity = new Vector2(horizontalInput * speed, rbody.velocity.y);
 
         // flip player if they move left or right
-        if(horizontalInput > 0.01f) // moving right
+        if (horizontalInput > 0.01f) // moving right
         {
-            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
         else if (horizontalInput < -0.01f) // moving left
         {
-            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
         }
+
              
         // check if space bar is used to jump and if on the ground
         if (Input.GetKey(KeyCode.Space) && IsGrounded())
         {
             Jump();
         }
+
+        // Check for when to switch from idle to running and vice versa - Matias
+        UpdateAnimation();
+
     }
 
     private void Jump()
@@ -66,6 +80,23 @@ public class PlayerMovement : MonoBehaviour
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
         // checks if anything is under the object (false if nothing, true if the ground is under/raycastHit)
         return raycastHit.collider != null;
+    }
+
+    // Checks to see if character is running or not so Animator can select the correct animation - Matias
+    private void UpdateAnimation()
+    {
+        if (horizontalInput > 0.01f) // moving right
+        {
+            anim.SetBool("Running", true);
+        }
+        else if (horizontalInput < -0.01f) // moving left
+        {
+            anim.SetBool("Running", true);
+        }
+        else // Not Moving
+        {
+            anim.SetBool("Running", false);
+        }
     }
 }
 
